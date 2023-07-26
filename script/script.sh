093f675033878a669ab295f3815b831f6cd8dd65
PanchoPotente <131989223+PanchoPotente@users.noreply.github.com>
2023-07-26 12:16:53 -0500

    script

--- script/script.sh
+++ script/script.sh
@@ -4,8 +4,10 @@ while true; do
 read -p "
 Lista de comandos:
 run: ejecutar proyecto
+slides: compilar y generar el pdf del proyecto latex relativo a la presentación
 report: compilar y generar el pdf del proyecto latex relativo al informe
 show_report: ejecutar un programa para visualizar el informe (normal lo ejecuta por defecto custom permite elegir comando de ejecucion)
+show_slides: ejecutar un programa que permita visualizar la presentación, si el fichero en pdf no ha sido generado debe generarse. Esta opción puede recibir puede recibir como parámetro el comando de la herramienta de visualización que se quiera utilizar, aunque debe tener una por defecto
 clean: eliminar todos los ficheros auxiliares que no forman parte del repositorio
 exit: salir del script
 Inserte un comando: " C
@@ -15,6 +17,33 @@ case $C in
 cd ./presentacion
 pdflatex presentacion.tex
 cd ..
+;;
+"show_slides")
+read -p "normal or custom" S
+echo $S
+
+case $S in
+"normal")
+cd ./presentacion
+if [ -e presentacion.pdf ]; then
+start presentacion.pdf 
+else
+pdflatex presentacion.tex
+start presentacion.pdf
+fi
+;;
+"custom")
+cd ./presentacion
+if [ -e presentacion.pdf ]; then
+read -p "ingrese su comando de ejecucion de pdf " F
+echo $F
+$F presentacion.pdf
+else
+pdflatex presentacion.tex
+read -p "ingrese su comando de ejecucion de pdf " F
+echo $F
+$F presentacion.pdf
+fi 
 ;;
 "report")
 cd ./informe
@@ -48,7 +77,7 @@ echo $S
 case $S in 
 "normal")
 cd ./informe
-if[ -o 121-Lázaro-Javier-Aragón-García.pdf ]; then
+if[ -e 121-Lázaro-Javier-Aragón-García.pdf ]; then
 start 121-Lázaro-Javier-Aragón-García.pdf     
 else
 pdflatex 121-Lázaro-Javier-Aragón-García.tex
